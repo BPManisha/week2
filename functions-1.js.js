7e55576b76a5df08ec9a46ebb3820518ec615680
BPManisha <107884131+BPManisha@users.noreply.github.com>
2022-06-28 21:17:22 -0400

    Add files via upload

---
+++ functions-1.js.js
@@ -0,0 +1,27 @@
+console.log(JonSnowHealth)
+console.log(JonSnowHealth)
+
+console.log(JonSnowHealth)
+let JonSnowHealth=string(100)
+console.log(JonSnowHealth)
+
+let thewinner=`Jamie is the winner`
+
+let Newwinner= thewinnerIs.toLowerCase().replace(`Jamie`,`Jon`)
+console.log(thewinner)
+console.log(thewinnerIs)
+
+
+newwinner= newwinner.replace("","-")
+newwinnerlower= newwinner.toLowerCase()
+newwinnersplit=newwinnerlower.split(' ')
+newwinnerKabob=newwinnersplit.join('-')
+
+console.log(newwinner,'-----------',newwinnerKabob)
+
+//create a function to see if Jon is alive
+function IsJonAlive(){
+  if JonSnowHealth> 0
+   
+}
+
