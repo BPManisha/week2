4a3bb8163e9b64a657755a11ad0da3965e70f6a3
BPManisha <107884131+BPManisha@users.noreply.github.com>
2022-06-30 22:56:55 -0400

    Add files via upload

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,20 @@
 // Write your code below
+function checkPalindrome(string) {
+
+  // find the length of a string
+  const len = string.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+
+      // check if first and last string are same
+      if (string[i] !== string[len - 1 - i]) {
+          return 'It is not a palindrome';
+      }
+  }
+  return 'It is a palindrome';
+}
+const string = "peeP"
+const value = checkPalindrome(string);
+
+console.log(value);
